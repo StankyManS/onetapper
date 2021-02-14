66a6c38d55c3177d11e848476cd59341e61edd3c
0xWrath <78025773+0xWrath@users.noreply.github.com>
2021-02-14 11:31:29 -0500

    Add files via upload

---
+++ toolz/GetFonts/GetFonts/GetFonts.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <shlobj_core.h>
+
+int main()
+{
+    char path[_MAX_PATH];
+    if (SHGetSpecialFolderPathA(nullptr, path, CSIDL_FONTS, 0)) {
+        std::vector<std::string> fontNames;
+
+        try {
+            for (auto& file : std::filesystem::directory_iterator(path)) {
+                if (file.path().extension() == ".ttf" || file.path().extension() == ".TTF")
+                    fontNames.push_back(file.path().filename().string());
+            }
+        }
+        catch (std::filesystem::filesystem_error err) {
+            std::cerr << err.what() << std::endl;
+            return 1;
+        }
+
+        std::string arrayString{ "[" };
+        int index{0};
+        for (auto it = fontNames.begin(); it != fontNames.end(); ++it, ++index)
+        {
+            if (*it != fontNames.back()) {
+                arrayString += "\""+ *it + "\",";
+            }
+            else
+                arrayString += "\"" + *it + "\"]";
+        }
+        std::cout << arrayString;
+    }   
+    else {
+        std::cerr << "SHGetSpecialFolderPathA failed" << std::endl;
+        return 1;
+    }
+    std::cin.get();
+}
+
